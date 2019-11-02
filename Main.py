36e37948073dcf4bba2173c02f9574197f0237b6
kelvin926 <kelvin926@naver.com>
2019-11-02 12:01:39 +0900

    Ver 2.0B

--- Main.py
+++ Main.py
@@ -173,7 +173,7 @@ for frame1 in camera.capture_continuous(rawCapture, format="bgr", use_video_port
 
     # 키보드의 Q 버튼을 통해 종료
     if cv2.waitKey(1) == ord('q'):
-        cv2.putText(frame, "사고가 감지되었습니다. 119서버와 연결중입니다.", (0, 100), font, 1, (255, 0, 0), 2, cv2.LINE_AA)
+        cv2.putText(frame, "WARNING", (0, 100), font, 1, (255, 0, 0), 2, cv2.LINE_AA)
         #break
 
     rawCapture.truncate(0)  # 다음 프레임 준비 클리어 작업(?)
